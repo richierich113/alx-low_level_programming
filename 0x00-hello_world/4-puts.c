b9ec3550bb68b1325d16076b07ba2d6e6b786b9d
Richard Boakye Duah <richrichie784@gmail.com>
2023-02-16 12:52:51 -0800

    Fixes 4-puts.c file

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,12 +1,10 @@
 #include <stdio.h>
 /**
- * main - a program that prints a line with puts funstion followed by a new line
+ * main - a program that prints a line with puts function followed by a new line
  * Return: Always 0 (Success)
  */
 int main(void)
 {
-puts("\"Programming is like building a multilingual puzz
-le");
-return (0);
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
 }
-
