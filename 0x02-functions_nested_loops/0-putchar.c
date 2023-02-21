ebd60201ca3cdc95cb6f4ba6c597a21259fe2efd
Richard Boakye Duah <richrichie784@gmail.com>
2023-02-21 03:54:20 -0800

    Adds 0-putchar.c and main.h file which contains header file.

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * main - Prints _putchar, followed by a new line.
+ *
+ * Description: Prints _putchar using _putchar function,
+ * followed by a new line.
+ * Return: 0
+ */
+int main(void)
+{
+	_putchar('_');
+	_putchar('p');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
+	_putchar('\n');
+
+	return (0);
+}
---
+++ 0x02-functions_nested_loops/README.md
@@ -0,0 +1,2 @@
+0x02. C - Functions, nested loops Project
+The 0-putchar.c is a program that prints _putchar, followed by a new line.
