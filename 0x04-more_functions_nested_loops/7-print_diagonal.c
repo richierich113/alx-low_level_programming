0e5f05b83a1f0b574e7bdc25830797e71dba95ac
Richard Boakye Duah <richrichie784@gmail.com>
2023-02-23 08:29:21 -0800

    Adds 7-print_diagonal.c

--- 0x04-more_functions_nested_loops/7-print_diagonal.c
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -7,25 +7,23 @@
  */
 void print_diagonal(int n)
 {
-	int i;
-
 	if (n <= 0)
 	{
 		_putchar('\n');
-	}
-	else
+	} else
 	{
-		i = 0;
+		int i, j;
 
-		while (i < n)
+		for (i = 0; i < n; i++)
 		{
-			_putchar(32);
-
-			i++;
+			for (j = 0; j < n; j++)
+			{
+				if (j == i)
+					_putchar('\\');
+				else if (j < i)
+					_putchar(' ');
+			}
+			_putchar('\n');
 		}
-
-		_putchar(92);
-		_putchar('\n');
-
 	}
 }
