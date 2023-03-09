e5aecbb2f4359c3696ceac1136a646080ef6ff9c
Richard Boakye Duah <richrichie784@gmail.com>
2023-03-08 17:26:20 -0800

    Adds 1-print_rev_recursion.c

---
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ *  _print_rev_recursion - print reversed characters
+ *  @s: string characters
+ */
+
+void _print_rev_recursion(char *s)
+{
+	if (*s != '\0')
+	{
+		_print_rev_recursion(s + 1);
+		_putchar(*s);
+	}
+}
--- 0x08-recursion/README.md
+++ 0x08-recursion/README.md
@@ -1,2 +1,3 @@
 0x08-recursion
 The 0-puts_recursion.c is a function that prints a string, followed by a new line.
+The 1-print_rev_recursion.c  is a function that prints a string in reverse.
