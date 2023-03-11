9e28b44715b334218176141d182495e13e9a111e
Richard Boakye Duah <richrichie784@gmail.com>
2023-03-10 16:21:17 -0800

    created object files and fed them into the libmy.a static library

--- 0x09-static_libraries/3-puts.c
+++ 0x09-static_libraries/3-puts.c
@@ -5,11 +5,11 @@
  * @str: pointer to character
  */
 
-void _puts(char *str)
+void _puts(char *s)
 {
-	while (*str != '\0')
+	while (*s != '\0')
 	{
-		_putchar(*str++);
+		_putchar(*s++);
 	}
 	_putchar('\n');
 }
