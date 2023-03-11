5f45aae0fbd042c64d4406218b82902e98459e78
Richard Boakye Duah <richrichie784@gmail.com>
2023-03-10 18:15:18 -0800

    Adds 1-args.c

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * main - print argument count
+ * @argc: arg count
+ * @argv: array of pointers to arg values
+ * Return: 0
+ */
+
+int main(int argc, char *argv[])
+{
+	argv = argv;
+	printf("%d\n", argc - 1);
+	return (0);
+}
--- 0x0A-argc_argv/README.md
+++ 0x0A-argc_argv/README.md
@@ -1,1 +1,2 @@
 0x0A-argc_argv
+The 1-args.c is a program that prints the number of arguments passed into it.
