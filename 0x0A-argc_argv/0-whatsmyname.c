6bb24636ad62358ab967f074fc726f7ee358e2f7
Richard Boakye Duah <richrichie784@gmail.com>
2023-03-10 18:10:02 -0800

    Adds 0-whatsmyname.c

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * main - print program name
+ * @argc: arg count
+ * @argv: array of pointers to arg values
+ * Return: 0
+ */
+
+int main(int argc, char *argv[])
+{
+	argc = argc;
+	printf("%s\n", argv[0]);
+	return (0);
+}
