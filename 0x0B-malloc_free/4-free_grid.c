667a6619390719d58da2f00536250acb36975b51
Richard Boakye Duah <richrichie784@gmail.com>
2023-03-15 22:27:01 -0700

    Adds 4-free_grid.c

---
+++ 0x0B-malloc_free/4-free_grid.c
@@ -0,0 +1,18 @@
+#include <stdlib.h>
+#include "main.h"
+
+/**
+ * free_grid - frees 2d grid
+ * @grid: 2d grid
+ * @height: grid height
+ */
+
+void free_grid(int **grid, int height)
+{
+	int i;
+
+	for (i = 0; i < height; i++)
+		free(grid[i]);
+
+	free(grid);
+}
