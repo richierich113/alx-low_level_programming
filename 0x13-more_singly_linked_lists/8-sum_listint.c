79c6b6806a6f0d2620541f576e66b5102aa1e439
Richard Boakye Duah <richrichie784@gmail.com>
2023-04-04 22:22:35 -0700

    Adds 8-sum_listint.c file

---
+++ 0x13-more_singly_linked_lists/8-sum_listint.c
@@ -0,0 +1,20 @@
+#include "lists.h"
+
+/**
+ * sum_listint - this sums up all the data in a given linked list
+ * @head: this is the head of the list
+ *
+ * Return: sum of the number
+ */
+int sum_listint(listint_t *head)
+{
+	listint_t *cursor = head;
+	size_t sum = 0;
+
+	while (cursor != NULL)
+	{
+		sum += cursor->n;
+		cursor = cursor->next;
+	}
+	return (sum);
+}
