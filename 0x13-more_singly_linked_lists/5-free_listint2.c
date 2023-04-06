9953077ac37c74c8b7730e15ed0e8338acb82540
Richard Duah Boakye <richrichie784@gmail.com>
2023-04-06 05:49:43 +0000

    Update 5-free_listint2.c

--- 0x13-more_singly_linked_lists/5-free_listint2.c
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -1,10 +1,11 @@
 #include "lists.h"
 
 /**
- *free_listint2 -this  frees a linked list
- *@head: a pointer to the head of the list
+ * free_listint2 -this  frees a linked list
+ * Description - A function that frees a linked list.
+ * @head: a pointer to the head of the list
  *
- *Return: void
+ * Return: void
  */
 void free_listint2(listint_t **head)
 {
