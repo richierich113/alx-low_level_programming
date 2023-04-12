b86a4f3de36dea39bc60b7a93e19cc646f286fe9
Richard Boakye Duah <richrichie784@gmail.com>
2023-04-11 23:19:17 -0700

    Updates main.h

--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -11,7 +11,7 @@
 int _putchar(char c);
 ssize_t read_textfile(const char *filename, size_t letters);
 int create_file(const char *filename, char *text_content);
-int _strlen(char *str);
+int get_strlen(char *string);
 int append_text_to_file(const char *filename, char *text_content);
 
 #endif /*MAIN_H*/
