d2e4d9d2d208c8aa063bba354a5c555503ec3513
Richard Boakye Duah <richrichie784@gmail.com>
2023-04-11 18:28:07 -0700

    Adds 100-get_endianness.c

---
+++ 0x14-bit_manipulation/100-main.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+  * main - This checks if the code of the get_endianness function
+  * works well.
+  *
+  * Description: This main function checks the get_endianness function
+  * code works.
+  * Return: Always 0.
+  */
+int main(void)
+{
+	int n;
+
+	n = get_endianness();
+	if (n != 0)
+	{
+		printf("Little Endian\n");
+	}
+	else
+	{
+		printf("Big Endian\n");
+	}
+	return (0);
+}
--- 0x14-bit_manipulation/README.md
+++ 0x14-bit_manipulation/README.md
@@ -5,3 +5,4 @@ get_bit is a function that returns the value of a bit at a given index.
 set_bit is a function that sets the value of a bit to 1 at a given index.
 clear_bit is a function that sets the value of a bit to 0 at a given index.
 flip_bits is a function that returns the number of bits you would need to flip to get from one number to another.
+get_endianness is a function that checks the endianness.
