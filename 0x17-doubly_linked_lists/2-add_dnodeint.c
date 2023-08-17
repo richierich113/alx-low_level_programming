9072abb2bee56eb91ea2f928ae39d4242e9d7f86
Richard Duah Boakye <richrichie784@gmail.com>
2023-08-17 21:38:03 +0000

    Update 2-add_dnodeint.c

--- 0x17-doubly_linked_lists/2-add_dnodeint.c
+++ 0x17-doubly_linked_lists/2-add_dnodeint.c
@@ -11,7 +11,7 @@ dlistint_t *add_dnodeint(dlistint_t **head, const int n)
 {
 	dlistint_t *new_node;
 	new_node = malloc(sizeof(dlistint_t));
-	if(!new_node)
+	if (!new_node)
 	{
 		return (NULL);
 	}
@@ -20,7 +20,7 @@ dlistint_t *add_dnodeint(dlistint_t **head, const int n)
 	new_node->prev = NULL;
 	new_node->next = *head;
 
-	if(*head != NULL)
+	if (*head != NULL)
 	{
 		(*head)->prev = new_node;
 	}
