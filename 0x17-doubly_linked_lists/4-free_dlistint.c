3a1e42407244cae589b6f16daaf1f7c38201d6d2
Richard Duah Boakye <richrichie784@gmail.com>
2023-08-18 00:26:32 +0000

    Update 4-free_dlistint.c

--- 0x17-doubly_linked_lists/4-free_dlistint.c
+++ 0x17-doubly_linked_lists/4-free_dlistint.c
@@ -5,7 +5,8 @@
  * at a specific index.
  * @head: The head of the dlistint_t list.
  */
- void free_dlistint(dlistint_t *head)
+void free_dlistint(dlistint_t *head)
+{
 	dlistint_t *tmp;
 
 	while (head != NULL)
