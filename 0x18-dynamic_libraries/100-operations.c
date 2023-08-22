155b272ce1c089a80702bf574216db649fcb6e50
Richard Boakye Duah <richrichie784@gmail.com>
2023-08-22 04:56:41 -0700

    Creates the shared library, 100-operations.so

---
+++ 0x18-dynamic_libraries/100-operations.c
@@ -0,0 +1,72 @@
+/**
+ * add - adds x and y values
+ * @x: integer parameter
+ * @y: integer parameter
+ * Return: sum of x and y
+ */
+
+int add(int x, int y)
+{
+	int sum;
+
+	sum = x + y;
+	return (sum);
+}
+
+/**
+ * sub - subtracts x from y
+ * @x: integer parameter
+ * @y: integer parameter
+ * Return: difference
+ */
+
+int sub(int x, int y)
+{
+	int diff;
+
+	diff = x - y;
+	return (diff);
+}
+
+/**
+ * mul - multiply x and y
+ * @x: integer parameter
+ * @y: integer parameter
+ * Return: product of x and y
+ */
+
+int mul(int x, int y)
+{
+	int product;
+
+	product = x * y;
+	return (product);
+}
+/**
+ * div - divide x by y
+ * @x: integer parameter
+ * @y: integer parameter
+ * Return: quotient
+ */
+
+int div(int x, int y)
+{
+	int quotient;
+
+	quotient = x / y;
+	return (quotient);
+}
+/**
+ * mod - mods x by y
+ * @x: integer parameter
+ * @y: integer parameter
+ * Return: modulus
+ */
+
+int mod(int x, int y)
+{
+	int modulus;
+
+	modulus = x % y;
+	return (modulus);
+}
