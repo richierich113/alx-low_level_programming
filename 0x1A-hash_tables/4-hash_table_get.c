13aa802268b99a40eaec7b16baa054921df2485c
Richard Duah Boakye <richrichie784@gmail.com>
2023-09-08 20:22:12 +0000

    Update 4-hash_table_get.c

--- 0x1A-hash_tables/4-hash_table_get.c
+++ 0x1A-hash_tables/4-hash_table_get.c
@@ -18,10 +18,10 @@ char *hash_table_get(const hash_table_t *ht, const char *key)
 
 	index = key_index((unsigned char *)key, ht->size);
 	current = ht->array[index];
-	while (current) {
-		if (strcmp(current->key, key) == 0) {
+	while (current)
+	{
+		if (strcmp(current->key, key) == 0)
 			return (current->value);
-		}
 		current = current->next;
 	}
 	return (NULL);
