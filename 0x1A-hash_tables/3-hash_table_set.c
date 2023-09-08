f7545509c10b295f265dd0eb08d8aeb2c4dfaf20
Richard Duah Boakye <richrichie784@gmail.com>
2023-09-08 19:53:51 +0000

    Update 3-hash_table_set.c

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -11,7 +11,6 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 {
 	hash_node_t *newkv_pair;
 	unsigned long int index;
-	char *value_copy;
 
 	newkv_pair = (hash_node_t *)malloc(sizeof(hash_node_t));
 	if (newkv_pair == NULL)
