c33edb9a723868427cd4a3886a74925592c420b7
Richard Duah Boakye <richrichie784@gmail.com>
2023-09-09 02:33:32 +0000

    Update 6-hash_table_delete.c

--- 0x1A-hash_tables/6-hash_table_delete.c
+++ 0x1A-hash_tables/6-hash_table_delete.c
@@ -20,7 +20,7 @@ void hash_table_delete(hash_table_t *ht)
 			{
 				node = ht->array[i];
 				while (node)
-				{	
+				{
 					next_node = node->next;
 					if (node->value)
 						free(node->value);
