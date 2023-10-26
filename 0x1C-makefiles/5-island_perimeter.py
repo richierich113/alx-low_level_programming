23d0d5487804e00086c9c50c1782be971a212e47
Richard Duah Boakye <richrichie784@gmail.com>
2023-10-26 00:11:33 +0000

    Update 5-island_perimeter.py

--- 0x1C-makefiles/5-island_perimeter.py
+++ 0x1C-makefiles/5-island_perimeter.py
@@ -1,31 +1,39 @@
 #!/usr/bin/python3
-"""Module that calculates the perimeter of an island in a grid."""
+"""Module that calculates the perimeter of an island in a grid
+"""
 
+
+def water_area(grid, m, n):
+    """Returns the number of water neighbors in a grid
+    """
+    num = 0
+    if m > 0 and not grid[m - 1][n]:
+        num += 1
+    if j > 0 and not grid[m][n - 1]:
+        num += 1
+    if n < len(grid[m]) - 1 and not grid[m][n + 1]:
+        num += 1
+    if m < len(grid) - 1 and not grid[m + 1][n]:
+        num += 1
+    return num
 
 def island_perimeter(grid):
-    """Returns the perimeter of the island in grid."""
-    if not grid:
-        return 0
-
+    """Returns the perimeter of island"""
     perimeter = 0
+    for cell in range(len(grid)):
+        for j in range(len(grid[cell])):
+            if grid[cell][j]:
+                perimeter += water_area(grid, cell, j)
 
-    def dfs(i, j):
-        if i < 0 or i >= len(grid) or j < 0 or j >= len(grid[0])\
-                or grid[i][j] == 0:
-            return 1
-        if grid[i][j] == -1:
-            return 0
-
-        grid[i][j] = -1  # Mark as visited
-        neighbors = [(1, 0), (-1, 0), (0, 1), (0, -1)]
-
-        for di, dj in neighbors:
-            perimeter += dfs(i + di, j + dj)
-
-        return 0
-
-    for i in range(len(grid)):
-        for j in range(len(grid[i]):
-            if grid[i][j] == 1:
-                perimeter += dfs(i, j)
     return perimeter
+
+
+if __name__ == "__main__":
+    grid = [
+        [0, 0, 0, 0, 0, 0],
+        [0, 1, 0, 0, 0, 0],
+        [0, 1, 0, 0, 0, 0],
+        [0, 1, 1, 1, 0, 0],
+        [0, 0, 0, 0, 0, 0]
+    ]
+    print("The perimeter of the island is:", island_perimeter(grid))
