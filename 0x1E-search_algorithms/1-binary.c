f30250770d41990634c3c99cad268b87f5d5f1b9
Richard Duah Boakye <richrichie784@gmail.com>
2024-04-11 23:13:25 +0000

    Update 1-binary.c

--- 0x1E-search_algorithms/1-binary.c
+++ 0x1E-search_algorithms/1-binary.c
@@ -1,20 +1,13 @@
 #include "search_algos.h"
 
 /**
- * binary_search - a function that searches for a value in a sorted
- * array of integers using the Binary search algorithm
- * @array: is a pointer to the first element of the array to search in
- * @size: is the number of elements in array
- * @value: is the value to search for
- * Return: the index where value is located, if value is not
- * present in array or if array is NULL, your function must return -1
- */
-
-/*
- * You can assume that array will be sorted in ascending order
- * You can assume that value won’t appear more than once in array
- * You must print the array being searched every time it changes.
- * (e.g. at the beginning and when you search a subarray) (See example)
+ * binary_search - Searches for a value in a sorted array of integers
+ * using the Binary search algorithm
+ * @array: A pointer to the first element of the array to search in
+ * @size: The number of elements in array
+ * @value: The value to search for
+ *
+ * Return: The index where value is located, or -1 if not found
  */
 
 int binary_search(int *array, size_t size, int value)
@@ -25,37 +18,36 @@ int binary_search(int *array, size_t size, int value)
 	size_t i;
 	int Am;
 
-	if (array == NULL) /* self explanatory */
+	if (array == NULL)
 		return (-1);
 
 	while (L <= R)
 	{
-		Am = *(array + m); /* array value @ median adjusted at each iteration */
-		/* print the array being searched */
+		Am = *(array + m);
 		printf("Searching in array: ");
 		for (i = L; i < (R + 1); i++)
 		{
 			printf("%ld", i);
 			if (i < R)
-				printf(" "); /* if not last value use space as separator */
+				printf(" ");
 			else if (i == R)
-				printf("\n"); /* if last value terminate w/ \n */
+				printf("\n");
 		}
 
 		if (Am < value) /* median < target */
 		{
-			L = m + 1; /* first index shifts to median + 1 */
-			m = (L + R) / 2; /* the median is recomputed */
+			L = m + 1;
+			m = (L + R) / 2;
 		}
-		else if (Am > value) /* value < median */
+		else if (Am > value)
 		{
-			R = m - 1; /* last index shifts to median - 1 */
-			m = (L + R) / 2; /* the median is recomputed */
+			R = m - 1;*/
+			m = (L + R) / 2;
 		}
 		else if (Am == value)
 		{
-			return (m); /* median is target */
+			return (m);
 		}
 	}
-	return (-1); /* if array exhausted */
+	return (-1);
 }
